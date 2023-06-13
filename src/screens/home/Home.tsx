34e8c02c1fc119f74ee4c769b4a6fa93a2633891
amilgasimov <93605485+amilgasimov@users.noreply.github.com>
2023-06-13 05:28:30 +0400

    home ended

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -12,6 +12,7 @@ import CoffeeCat from '../../components/CoffeeCat';
 import BeerCat from '../../components/BeerCat';
 import WineCat from '../../components/WineCat';
 import WhiskeyCat from '../../components/WhiskeyCat';
+import TextButton from '../../components/TextButton';
 
 const Home = () => {
     const dispatch = useDispatch<AppDispatch>()
@@ -64,6 +65,46 @@ const Home = () => {
                         </TouchableOpacity>
                     )}
                 />
+                <View style={styles.rows}>
+                    <Text style={styles.cardName}>Frozen Beverages</Text>
+                    <TextButton title="See All" onPress={{}} />
+                </View>
+                <FlatList
+                    data={coffeeData}
+                    keyExtractor={(item) => item.id.toString()}
+                    horizontal
+                    renderItem={({ item }) => (
+                        <TouchableOpacity style={styles.cardContainerLarge}>
+                            <View style={styles.cardContent}>
+                                <View style={styles.cardInfo}>
+                                    <Text style={styles.cardName}>{item.coffeeName}</Text>
+                                    <Text style={styles.cardPrice}>{item.price}</Text>
+                                </View>
+                                <Image style={styles.cardImageLarge} source={{ uri: item.imageURL }} />
+                            </View>
+                        </TouchableOpacity>
+                    )}
+                />
+                <View style={styles.rows}>
+                    <Text style={styles.cardName}>Kava and Bottled Beverages</Text>
+                    <TextButton title="See All" onPress={{}} />
+                </View>
+                <FlatList
+                    data={coffeeData}
+                    keyExtractor={(item) => item.id.toString()}
+                    horizontal
+                    renderItem={({ item }) => (
+                        <TouchableOpacity style={styles.cardContainerLarge}>
+                            <View style={styles.cardContent}>
+                                <View style={styles.cardInfo}>
+                                    <Text style={styles.cardName}>{item.coffeeName}</Text>
+                                    <Text style={styles.cardPrice}>{item.price}</Text>
+                                </View>
+                                <Image style={styles.cardImageLarge} source={{ uri: item.imageURL }} />
+                            </View>
+                        </TouchableOpacity>
+                    )}
+                />
             </ScrollView>
         </ImageBackground>
     );
@@ -72,6 +113,35 @@ const Home = () => {
 export default Home;
 
 const styles = StyleSheet.create({
+    cardContainerLarge: {
+        width: 190,
+        height: 100,
+        marginRight: 10,
+        backgroundColor: '#272220',
+        borderRadius: 12,
+    },
+    cardContent: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 10,
+    },
+    cardInfo: {
+        flex: 1,
+    },
+    cardImageLarge: {
+        width: 45,
+        height: 70,
+        borderRadius: 10,
+        marginLeft: 10,
+    },
+    rows: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginHorizontal: 10,
+        marginVertical:12,
+    },
     backgroundImage: {
         flex: 1,
         width: '100%',
